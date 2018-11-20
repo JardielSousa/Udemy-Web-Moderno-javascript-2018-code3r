ecfae1eb0ab55e6ecaf12346b5d075a808912ee9
Jardiel Sousa <jardiel@infoway-pi.com.br>
2018-11-20 13:35:53 -0300

    Tipos em JavaScript: Object

---
+++ fundamentos/tipos-javascript-object.js
@@ -0,0 +1,16 @@
+const prod1 = {}
+console.log(typeof prod1);
+
+prod1.nome = 'Celular Megaaa'
+prod1.preco = 4321.90
+prod1['desconto_legal'] = 0.40
+
+console.log(prod1)
+
+const prod2 = {
+    nome: 'Camisa',
+    preco: 89.01
+}
+console.log(typeof prod2);
+
+console.log(prod2);
