06a53318f2b5fa9366ad4cccb806ddde39c57199
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-01-07 13:49:07 -0300

    Tipos de Declaração

---
+++ funcao/tipos-declaracao.js
@@ -0,0 +1,16 @@
+console.log(soma(1,2))
+
+// Function declaration
+function soma(a,b) {
+  return a+b
+}
+
+// Function expression
+const sub = function(x,y) {
+  return x-y
+}
+
+// Named function expression
+const mult = function mult(a,b) {
+  return a*b
+}
