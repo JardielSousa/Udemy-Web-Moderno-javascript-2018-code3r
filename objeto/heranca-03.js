91300772d1bc54b436133f7d39e6e27c9cbdbd0f
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-01-11 14:03:37 -0300

    Herança

---
+++ objeto/heranca-03.js
@@ -0,0 +1,27 @@
+const pai = {
+  nome: "Papai",
+  corCabelo: "preto"
+}
+
+const filha1 = Object.create(pai)
+filha1.nome = "Laila"
+console.log(filha1.corCabelo)
+
+const filha2 = Object.create(pai, {
+  nome: {
+    value: "Filha 02",
+    writable: false,
+    enumerable: true
+  }
+})
+
+console.log(filha2.nome)
+filha2.nome = "Filha 03"
+console.log(`${filha2.nome} tem cabelo ${filha2.corCabelo}`)
+
+console.log(Object.keys(filha1))
+console.log(Object.keys(filha2))
+
+for(let key in filha2) {
+  filha2.hasOwnProperty(key) ? console.log(key) : console.log(`Por herança: ${key}`)
+}
