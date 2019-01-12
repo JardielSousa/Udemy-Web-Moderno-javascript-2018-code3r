651f0e70754edc2079e030cc1a79c25aea46cd85
Jardiel Sousa <jardiel.sousa@gmail.com>
2019-01-12 12:57:19 -0300

    Herança

---
+++ objeto/heranca-04.js
@@ -0,0 +1,27 @@
+function MeuObjeto() { }
+console.log(MeuObjeto.prototype)
+
+const obj1 = new MeuObjeto()
+const obj2 = new MeuObjeto()
+console.log(obj1.__proto__ === obj2.__proto__)
+console.log(MeuObjeto.prototype === obj1.__proto__)
+
+MeuObjeto.prototype.nome = "Ninguem"
+MeuObjeto.prototype.falar = function() {
+  console.log(`Meu nome é ${this.nome}`)
+}
+
+obj1.falar()
+obj2.nome = "Objeto 02"
+obj2.falar()
+
+const obj3 = {}
+obj3.__proto__ = MeuObjeto.prototype
+obj3.nome = "Objeto 03"
+obj3.falar()
+
+// Resumindo.....
+console.log((new MeuObjeto).__proto__ === MeuObjeto.prototype)
+console.log(MeuObjeto.__proto__ === Function.prototype)
+console.log(Function.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
