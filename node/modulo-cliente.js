fb56b99bca8a7751dda97ae7f79481201756b71c
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-01-17 14:16:29 -0300

    Sistema de modulos

---
+++ node/modulo-cliente.js
@@ -0,0 +1,11 @@
+const moduloA = require("./modulo-a")
+const moduloB = require("./modulo-b")
+
+console.log(moduloA.ola)
+console.log(moduloA.bemVindo)
+console.log(moduloA.ateLogo)
+console.log(moduloA)
+console.log()
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
