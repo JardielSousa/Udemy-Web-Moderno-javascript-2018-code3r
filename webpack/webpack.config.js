4552a5b1db0cc306794bad07ed98f415041d0b9c
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-03-08 15:03:05 -0300

    webpack: desenvolvimento e produção

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,12 +1,25 @@
+const modoDev = process.env.NODE_ENV !== "production"
 const webpack = require("webpack")
 const miniCssExtractPlugin = require("mini-css-extract-plugin")
+// const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
+const optimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  mode: "development",
+  mode: modoDev ? "development" : "production",
   entry: "./src/principal.js",
   output: {
     filename: "principal.js",
     path: __dirname + "/public"
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        cache: true,
+        parallel: true
+      }),
+      new optimizeCssAssetsPlugin({})
+    ]
   },
   plugins: [
     new miniCssExtractPlugin({
