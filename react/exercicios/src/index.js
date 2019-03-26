0127146a1006524e85d852cf84103c73dbecfd62
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-03-26 13:50:03 -0300

    React componentes pai & filho

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom";
 
 // import Bomdia from "./componentes/Bomdia";
 // import { BoaTarde, BoaNoite } from "./componentes/Multiplos";
-import Saudacao from "./componentes/Saudacao";
+// import Saudacao from "./componentes/Saudacao";
+import Pai from "./componentes/Pai";
+import Filho from "./componentes/Filho";
 
 ReactDOM.render(
   <div>
-    <Saudacao tipo="Boa tarde" nome="Laila"/>
+    <Pai nome="Jardiel" sobrenome="Sousa">
+      <Filho nome="Laila Caroline" />
+      <Filho nome="Filho 02" />
+    </Pai>
   </div>
   , document.getElementById("root"))
