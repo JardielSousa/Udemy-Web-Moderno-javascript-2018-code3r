b298c3ac3a6d89fd043a3d6701667edccea9f825
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-03-26 12:24:40 -0300

    React múltiplos componentes

--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Bomdia from "./componentes/Bomdia";
+// import Bomdia from "./componentes/Bomdia";
+import { BoaTarde, BoaNoite } from "./componentes/Multiplos";
 
-ReactDOM.render(<Bomdia nome="Laila"/>, document.getElementById("root"))
+ReactDOM.render(
+  <div>
+    <BoaTarde nome="Laila"/>
+    <BoaNoite nome="Caroline"/>
+  </div>
+  , document.getElementById("root"))
