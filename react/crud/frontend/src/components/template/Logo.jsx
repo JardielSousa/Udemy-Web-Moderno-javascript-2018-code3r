dd23f5663dbbf9fb5986c0bf85adc789e2c7ad0a
Jardiel <jardiel.sousa@gmail.com>
2019-03-31 13:30:55 -0300

    React crud implementando rotas

--- react/crud/frontend/src/components/template/Logo.jsx
+++ react/crud/frontend/src/components/template/Logo.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Logo.css"
 import logo from "../../assets/imgs/logo.png"
 
 export default props => 
   <aside className="logo">
-    <img src={logo} alt="logo"/>
+    <Link to="/" className="logo">
+      <img src={logo} alt="logo"/>
+    </Link>
   </aside>
