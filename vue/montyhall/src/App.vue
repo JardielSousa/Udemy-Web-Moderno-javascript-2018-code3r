e29728476ec2357b547ea13e0678cf3cbe51ce22
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-04-11 13:46:58 -0300

    Vue Problema de Monty Hall

---
+++ vue/montyhall/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>Problema de Monte Hall</h1>
+    <Gift />
+  </div>
+</template>
+
+<script>
+  import Gift from "./components/Gift";
+  
+  export default {
+    name: "App",
+    components: { Gift }
+  }
+</script>
+
+<style>
+  * {
+    font-family: 'Montserrat', sans-serif;
+    box-sizing: border-box;
+  }
+
+  body {
+    color: #fff;
+    background: linear-gradient(to right, rgb(105, 212, 159), rgb(21, 87, 153))
+  }
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #app h1 {
+    border: 1px solid #000;
+    background-color: #0004;
+    padding: 20px;
+    margin-bottom: 60px;
+  }
+</style>
