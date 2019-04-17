7eed400bf39f60cee5967b9a980aa2ce3c98a075
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-04-17 13:16:28 -0300

    Express tipos de resposta

--- exercicios-express/index.js
+++ exercicios-express/index.js
@@ -2,7 +2,18 @@ const express = require("express")
 const app = express()
 
 app.use("/opa", (req, res) => {
-  res.send("Estou <b>legal</b>!")
+  // res.send("Estou <b>legal</b>!")
+  
+  // res.json({
+  //   name: "Extensão",
+  //   price: "2.0",
+  //   discount: "0.1"
+  // })
+
+  res.json([
+    {id: 2, name: "Laila"},
+    {id: 36, name: "Jardiel"}
+  ])
 })
 
 app.listen(3000, () => {
