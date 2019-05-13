1f5e1561aa63b3a0258c08d6607891b17797e750
Jardiel Sousa <jardiel@infoway-pi.com.br>
2019-05-13 14:06:02 -0300

    Projeto final Páginas Administrativas

--- knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
+++ knowledge/versao-inicial/frontend/src/components/admin/AdminPages.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="admin-pages">
     <PageTitle icon="fa fa-cogs" main="Administração do Sistema" sub="Cadastros & Cia"/>
-    <!-- <div class="admin-pages-tabs">
+    <div class="admin-pages-tabs">
       <b-card no-body>
         <b-tabs card>
           <b-tab title="Artigos" active>
@@ -15,20 +15,19 @@
           </b-tab>
         </b-tabs>
       </b-card>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script>
   import PageTitle from "../template/PageTitle";
-  // import ArticleAdmin from "./ArticleAdmin";
-  // import CategoryAdmin from "./CategoryAdmin";
-  // import UserAdmin from "./UserAdmin";
+  import ArticleAdmin from "./ArticleAdmin";
+  import CategoryAdmin from "./CategoryAdmin";
+  import UserAdmin from "./UserAdmin";
 
   export default {
     name: "AdminPages",
-    // components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
-    components: { PageTitle }
+    components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
   };
 </script>
 
